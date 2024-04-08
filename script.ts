c16b37343ad49bf74a3bb21ae4b70daa46dd8cf2
fanpeii <48142497+fanpeii@users.noreply.github.com>
2024-04-08 22:52:46 +0800

    Update script.ts

--- script.ts
+++ script.ts
@@ -2,22 +2,40 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice1@prisma.io',
-    },
-  })
-  console.log(user)
+async function main2() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'Alice',
+            email: 'alice1@prisma.io',
+        },
+    })
+    console.log(user)
 }
 
+async function main3() {
+    const users = await prisma.user.findMany()
+    console.log(users)
+}
+async function main() {
+    const user = await prisma.user.create({
+        data: {
+            name: 'Bob',
+            email: 'bob@prisma.io',
+            posts: {
+                create: {
+                    title: 'Hello World',
+                },
+            },
+        },
+    })
+    console.log(user)
+}
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
